28faded3432d20cc2e9b9e58b4706031bd6ea9c9
Yang Ni <yangni@google.com>
2017-02-21 16:40:01 -0800

    Added support for global variables
    Bug: 30964317
    
    Pass necessary metadata about global variables--i.e., offsets of each
    global variable in the synthesized global buffer--from the compiler to
    the driver. This metadata is encoded as an OpString in the generated
    SPIR-V code. The driver also creates the global buffer to store all
    global variables.
    
    Added support for sharing globals between invokes and kernels. rs2spirv
    also does global variable access to global buffer field access
    transformation for the CPU code, so that CPU and GPU access the same
    global buffer resident in shared memory assuming the same data layout.
    
    Added in the compiler support to handle global variable intializers,
    and contantant expressions.
    
    Removed capability Address, since Vulkan validation rules disallow it.
    Converted OpInBoundsPtrAccessChain into OpInBoundsAccessChain, since the
    former requires capability Address. OpInBoundsPtrAccessChain
    instructions get generated for accesses to global variables, which are
    now fields in the global buffer.
    
    Added an RSoV test for global variables. Adjusted lit tests to check
    the new OpString.
    
    This CL is limited to non-allocation global variables, as global
    rs_allocations are handled in separate CLs.
    
    This CL does not handle constant globals either, which will be dealt
    with in followup CLs.
    
    Test: lit tests and RSoVTest
    
    Change-Id: Ia8f44c4ffa22046a72a58a26942ced595b3f5f53

--- rsov/tests/RSoVTest/src/com/android/rs/rsov/test/global_float.rs
+++ rsov/tests/RSoVTest/src/com/android/rs/rsov/test/global_float.rs
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016, The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,17 +14,10 @@
  * limitations under the License.
  */
 
-#ifndef RS2SPIRV_GLOBAL_MERGE_PASS_H
-#define RS2SPIRV_GLOBAL_MERGE_PASS_H
+#pragma version(1)
 
-namespace llvm {
-class ModulePass;
-} // namespace llvm
+#pragma rs java_package_name(com.android.rs.rsov.test)
 
-namespace rs2spirv {
+#include "global.rsh"
 
-llvm::ModulePass *createGlobalMergePass(bool CPU = false);
-
-} // namespace rs2spirv
-
-#endif
+HANDLE_TYPE(float)
