5d955c9f301bb5acfdd5e22b69755e254f73984d
Miao Wang <miaowang@google.com>
2017-03-01 11:57:47 -0800

    Implement USAGE_IO_OUTPUT related methods on top of NDK.
     - Use ANativeWindow NDK APIs to implement Allocation.IoSend and
     Allocation.SetSurface.
    
    Bug: 34396220
    Test: mm, CTS tests pass and ImageProcessingJB work well.
    Change-Id: If7afe0ae5261f5ce56565ecd5df67e1ec3db800b

--- Android.mk
+++ Android.mk
@@ -64,7 +64,7 @@ LOCAL_SRC_FILES:= \
 
 LOCAL_SHARED_LIBRARIES += libRS_internal libRSCpuRef
 LOCAL_SHARED_LIBRARIES += liblog libutils libEGL libGLESv1_CM libGLESv2
-LOCAL_SHARED_LIBRARIES += libui libgui
+LOCAL_SHARED_LIBRARIES += libui libgui libandroid
 
 LOCAL_SHARED_LIBRARIES += libbcinfo
 
